0549da80a6d170c6e9625a67e878579a7178f7c9
noodled <3495329+noodled@users.noreply.github.com>
2022-03-31 18:10:02 +0200

    Add files via upload

--- inst-x2go-isolated-firefox-browser.sh
+++ inst-x2go-isolated-firefox-browser.sh
@@ -7,10 +7,10 @@ sudo apt-get autoremove -y
 sudo apt-get install --no-install-recommends -y xfce4 firefox evince eog libappindicator fonts-liberation gedit xfce4-screenshooter unzip
 sudo apt-get remove -y xscreensaver
 
-sudo add-apt-repository ppa:x2go/stable
+sudo add-apt-repository -y ppa:x2go/stable
 sudo apt-get -y update
 sudo apt-get install --no-install-recommends -y x2goserver x2goserver-xsession
-      
+
 pushd ~
 curlUA='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36'
 curlparam=--referer=www.google.com -L --ipv4 -#
@@ -26,8 +26,8 @@ mv /home/atkinson/.config /home/atkinson/.config-old
 
 echo "$sudolla ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/dont-prompt-$sudolla-for-sudo-password
 echo $sudolla will be your sudoer user
-    
-echo   Put customized config (Xfce panel setup, Chrome profile with smooth scrolling disabled)
+
+echo -e 'Put customized config (Xfce panel setup, Chrome profile with smooth scrolling disabled)'
 ## cp -r homedir/{{ item }} /home/atkinson && chown -R atkinson:atkinson /home/atkinson/{{ item }}"
 
 ## cp -r homedir/.config /home/atkinson && chown -R atkinson:atkinson /home/atkinson/.config
@@ -43,11 +43,11 @@ Exec=setxkbmap be
 StartupNotify=false
 Terminal=false
 EOF
- 
+
 echo enabling SSHd password auth
 sed -i s'/^#?PasswordAuthentication/PasswordAuthentication yes/'g /etc/ssh/sshd_config
 systemctl restart ssh
-    
+
 pushd ~
 curl -A "curlUA" "https://download.nomachine.com/download/7.9/Linux/nomachine_7.9.2_1_amd64.deb" $curlparam -o nomachine_7.9.2_1_amd64.deb
 sudo apt install -y ./nomachine_7.9.2_1_amd64.deb
@@ -57,4 +57,3 @@ sed -i s'@^DefaultDesktopCommand "/etc/X11/Xsession default"@DefaultDesktopComma
 sudo systemctl stop display-manager
 sudo /etc/NX/nxserver --restart
 sudo systemctl restart display-manager
-
